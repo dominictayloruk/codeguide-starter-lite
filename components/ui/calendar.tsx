f2878d7ec2750c1f30a7477198438816b4d7447a
Dominic Taylor <dominic@dominictaylor.dev>
2025-03-29 13:36:32 +0000

    fixed type error

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
@@ -52,14 +53,6 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
-      }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
       }}
       {...props}
     />
